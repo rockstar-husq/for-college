e444e238776291b12e868d9993596cab8a2d2c02
rockstar-husq <toprroo94@gmail.com>
2024-11-30 16:03:39 +0300

    Update pz4.py

--- PZ-4/pz4.py
+++ PZ-4/pz4.py
@@ -3,13 +3,19 @@
 
 print('Даны два целых числа A и B (A < B). Найти сумму квадратов всех целых чисел')
 print('от A до B включительно.')
+
+# Введем обработку исключений
 try:
     num_a = int(input('Input A number: '))
     num_b = int(input('Input B number: '))
     tsum = 0
+
+    # Введем условие проверки чисел
     if num_a > num_b:
         print('Некоректные значения!')
     else:
+
+        # Введем цикл while который будет расчитывать сумму всех квадратов
         while num_a <= num_b :
             psum = num_a ** 2
             num_a = num_a + 1
