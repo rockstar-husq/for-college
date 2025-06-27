081eb63bf6a3703bb3da582f5c59efd95ef2a46b
rockstar-husq <181490643+rockstar-husq@users.noreply.github.com>
2025-06-27 13:17:07 +0300

    Update PZ-14.py

--- PZ-14/PZ-14.py
+++ PZ-14/PZ-14.py
@@ -1,13 +1,19 @@
 # В исходном текстовом файле(Dostoevsky.txt) найти все произведения писателя.
 # Посчитать количество полученных элементов.
 
+import re
+p = re.compile('«([а-яА-Я\s]+)»')
+
 def find_works_in_file(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         content = file.read()
 
     # Предположим, что произведения разделены строками, начинающимися с "Название:"
     # Замените этот шаблон на тот, который соответствует вашему файлу
-    works = content.split("Название:")  # Измените на нужный разделитель
+    works = p.findall(content)
+
+    # убираем повторения
+    works = list(set(works))
 
     # Удаляем пустые строки и пробелы
     works = [work.strip() for work in works if work.strip()]
